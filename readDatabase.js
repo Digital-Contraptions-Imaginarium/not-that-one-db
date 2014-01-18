4f3a79684a3f4961eea7cd23b5ea3b39d5289443
Gianfranco Cecconi <gianfranco.cecconi@digitalcontraptionsimaginarium.co.uk>
2014-01-18 13:01:49 +0000

    wip

---
+++ readDatabase.js
@@ -0,0 +1,18 @@
+var readDatabase = function (callback) {
+    var tells = [ ];	
+    d3.json("./tells.json", function (tempTells) {
+        tells = tempTells.reduce(function (memo, tell) {
+            memo[tell.name] = tell;
+            return memo;
+        }, { });
+        async.each(Object.keys(tells), function (tellName, callback) {
+            d3.json("./tells/" + tellName + ".json", function (tellData) {
+                tells[tellName].products = tellData.products;
+                callback(!tellData.products);
+            });
+        }, function (err) {
+            console.log(JSON.stringify(database));
+            callback(err, { tells: tells });
+        });
+    });
+}
